12205e78a1936fcae8b4e5a8cd7b9fb633b40cda
Dean Lovett <dean.lovett@gmail.com>
2020-02-16 19:16:14 -0800

    adds gitignore, more refinements

--- ntw.js
+++ ntw.js
@@ -36,311 +36,71 @@ let tens={
     '9':'Ninety',
 };
 let millsd={
-    '1':'Hundred',
+    '0':'',
+    '1':'',
     '2':'Thousand',
     '3':'Million',
     '4':'Billion',
     '5':'Trillion',
 }
 
-
-
-
-
 function processData(input) {
-    //Enter your code here
-    // console.log(':::::::::::::::::::::::::::::::::::');
-    // console.log(':::::::::::::::::::::::::::::::::::');
-    // console.log('input:');
-    // console.log(input);
     let ia = input.split('\n');
     let count = parseInt(ia.shift(),10);
-    // console.log('count:',count);
 
     for(let i=0;i<count;++i){
-        // console.log('');
-        // console.log('====================');
-        // // console.log('num #:',i,'val:',ia[i]);
         processNum(ia[i]);
     }
 } 
 
 function processNum(snum){
     let ret='';
-    // console.log('processNum(',snum,')');
-    // console.log('string(',snum,')');
+
     snum = snum.replace(/\s+/g, '')
-    console.log('');
-    console.log('string(',snum,')');
+    // console.log('string(',snum,')');
     let mills= snum.length===1?1:Math.ceil((snum.length)/3);
     let subm=(snum.length-1)%3;
     // console.log('chunk by threes:',snum.length, mills, subm);
     let cur;
-    if(mills>=5){ //tills
-        cur  = snum.substring(0,subm+1);
-        snum = snum.substring(subm+1);
-        let n=parseInt(cur,10);
-        let pre='';
-        cur = ''+n;
-        // console.log(cur,cur.length);
-        switch(cur.length){
-            case 3:
+    if( parseInt(snum,10)===0){
+        console.log('Zero');
+        return;
+    }
+
+    for( let i=5;i>0;--i){
+        if(mills===i){ //tills
+            cur  = snum.substring(0,subm+1);
+            snum = snum.substring(subm+1);
+            let  n = parseInt(cur,10);
+            let dn = n%100;
+            let pre = '';
+            cur = '' + n;
+            if( cur.length === 3 ){
                 let h=cur.substring(0,1);
                 cur = cur.substring(1,3);
-                pre = ones[h]+' '+millsd[1];
-            case 2: case 1:
-                if(n===0){ }
-                else if(n<20){
-                    // // console.log(ones[cur],millsd[4]);
-                    // ret += ones[cur] +' '+ millsd[4];
-                    if( cur=='0')
-                        if( pre.length>0 )
-                            pre += ' '+ millsd[5];
-                        else
-                            pre  =      millsd[5];
-                    else
-                        if( pre.length>0 )
-                            pre += ' '+ ones[cur] +' '+ millsd[5];
-                        else
-                            pre  =      ones[cur] +' '+ millsd[5];
-                }else{
-                    let t = cur.substring(0,1);
-                    let s = cur.substring(1,2);
-                    // // console.log(tens[t],ones[s],millsd[4]);
-                    // ret += tens[t] +' '+ ones[s] +' '+ millsd[4];
-                    if( cur=='0')
-                        if( pre.length>0 )
-                            pre += ' '+ tens[t] +' '+ millsd[5];
-                        else
-                            pre  =      tens[t] +' '+ millsd[5];
-                    else
-                        if( pre.length>0 )
-                            pre += ' '+ tens[t] +' '+ ones[s] +' '+ millsd[5];
-                        else
-                            pre  =      tens[t] +' '+ ones[s] +' '+ millsd[5];
-
-                }
-                // console.log(pre);
-                break;
-            default:
-                // console.log('error: unexpected cur.length:', cur.length);
-                break;
-        }
-        if( ret.length>0)
-            ret += ' ' + pre;
-        else
-            ret = pre;
-        --mills;
-        subm=2;
-    }
-    if(mills>=4){ //bills
-        cur  = snum.substring(0,subm+1);
-        snum = snum.substring(subm+1);
-        let n=parseInt(cur,10);
-        let pre='';
-        cur = ''+n;
-
-        // console.log(cur,cur.length);
-        switch(cur.length){
-            case 3:
-                let h=cur.substring(0,1);
-                cur = cur.substring(1,3);
-                pre = ones[h]+' '+millsd[1];
-            case 2: case 1:
-                if(n===0){ }
-                else if(n<20){
-                    // // console.log(ones[cur],millsd[4]);
-                    // ret += ones[cur] +' '+ millsd[4];\
-                    if( cur=='0')
-                        if( pre.length>0 )
-                            pre += ' '+ millsd[4];
-                        else
-                            pre  =      millsd[4];
-                    else
-                        if( pre.length>0 )
-                            pre += ' '+ ones[cur] +' '+ millsd[4];
-                        else
-                            pre  =      ones[cur] +' '+ millsd[4];
-                }else{
-                    let t = cur.substring(0,1);
-                    let s = cur.substring(1,2);
-                    // // console.log(tens[t],ones[s],millsd[4]);
-                    // ret += tens[t] +' '+ ones[s] +' '+ millsd[4];
-                    if( cur=='0')
-                        if( pre.length>0 )
-                            pre += ' '+ tens[t] +' '+ millsd[4];
-                        else
-                            pre  =      tens[t] +' '+ millsd[4];
-                    else
-                        if( pre.length>0 )
-                            pre += ' '+ tens[t] +' '+ ones[s] +' '+ millsd[4];
-                        else
-                            pre  =      tens[t] +' '+ ones[s] +' '+ millsd[4];
-
-                }
-                // console.log(pre);
-                break;
-            default:
-                // console.log('error: unexpected cur.length:', cur.length);
-                break;
-        }
-        if( ret.length>0)
-            ret += ' ' + pre;
-        else
-            ret = pre;
-        --mills;
-        subm=2;
-    }
-    if(mills>=3){//mills
-        cur  = snum.substring(0,subm+1);
-        snum = snum.substring(subm+1);
-        let n=parseInt(cur,10);
-        let pre='';
-        cur = ''+n;
-        switch(cur.length){
-            case 3:
-                let h=cur.substring(0,1);
-                cur = cur.substring(1,3);
-                pre = ones[h]+' '+millsd[1];
-            case 2: case 1:
-                if(n===0){ 
-                    // console.log(pre);
-                }else if(n<20){ 
-                    if( cur=='0')
-                        if( pre.length>0 )
-                            pre += ' '+ millsd[3];
-                        else
-                            pre  =      millsd[3];
-                    else
-                        if( pre.length>0 )
-                            pre += ' '+ ones[cur] +' '+ millsd[3];
-                        else
-                            pre  =      ones[cur] +' '+ millsd[3];
-                }else{
-                    let t = cur.substring(0,1);
-                    let s = cur.substring(1,2);
-                    if( cur=='0')
-                        if( pre.length>0 )
-                            pre += ' '+ tens[t] +' '+ millsd[3];
-                        else
-                            pre  =      tens[t] +' '+ millsd[3];
-                    else
-                        if( pre.length>0 )
-                            pre += ' '+ tens[t] +' '+ ones[s] +' '+ millsd[3];
-                        else
-                            pre  =      tens[t] +' '+ ones[s] +' '+ millsd[3];
-                }
-                // console.log(pre);
-                break;
-            default:
-                // console.log('error: unexpected cur.length:', cur.length);
-                break;
-        }
-        if( ret.length>0)
-            ret += ' ' + pre;
-        else
-            ret = pre;
-        --mills;
-        subm=2;
-    }
-    if(mills>=2){//thous
-        cur  = snum.substring(0,subm+1);
-        snum = snum.substring(subm+1);
-        // console.log(cur, millsd[2]);
-        let n=parseInt(cur,10)%100;
-        let pre='';
-        cur = ''+n;
-        if( cur.length === 3 ){
-            let h=cur.substring(0,1);
-            cur = cur.substring(1,3);
-            pre = ones[h]+' '+millsd[1];
-        }
-        if(dn>0){
-            if(dn<20){ 
-                pre += cur=='0' ? '' : ((pre.length>0?' ':'') + ones[cur]);
-            }else{
-                let t = cur.substring(0,1);
-                let s = cur.substring(1,2);
-                pre += (pre.length>0?' ':'') + tens[t] + (s=='0' ? '' : (' '+ ones[s]));
+                pre = ones[h]+' Hundred';
             }
-        }
-        if( pre.length>0) pre += ' '+ millsd[2];
-
-        // switch(cur.length){
-        //     case 3:
-        //         let h=cur.substring(0,1);
-        //         cur = cur.substring(1,3);
-        //         pre = ones[h]+' '+millsd[1];
-        //     case 2: case 1:
-        //         if(n===0){ 
-        //             // console.log(pre);
-        //         }else if(n<20){ 
-        //             if( cur=='0')
-        //                 if( pre.length>0 )
-        //                     pre += ' '+ millsd[2];
-        //                 else
-        //                     pre  =      millsd[2];
-        //             else
-        //                 if( pre.length>0 )
-        //                     pre += ' '+ ones[cur] +' '+ millsd[2];
-        //                 else
-        //                     pre  =      ones[cur] +' '+ millsd[2];
-        //         }else{
-        //             let t = cur.substring(0,1);
-        //             let s = cur.substring(1,2);
-        //             if( cur=='0')
-        //                 if( pre.length>0 )
-        //                     pre += ' '+ tens[t] +' '+ millsd[2];
-        //                 else
-        //                     pre  =      tens[t] +' '+ millsd[2];
-        //             else
-        //                 if( pre.length>0 )
-        //                     pre += ' '+ tens[t] +' '+ ones[s] +' '+ millsd[2];
-        //                 else
-        //                     pre  =      tens[t] +' '+ ones[s] +' '+ millsd[2];
-        //         }
-        //         // console.log(pre);
-        //         break;
-        //     default:
-        //         // console.log('error: unexpected cur.length:', cur.length);
-        //         break;
-        // }
-        if( ret.length>0)
-            ret += ' ' + pre;
-        else
-            ret = pre;
-
-        --mills;
-        subm=2;
-    }
-    if(mills>=1){//less
-        cur  = snum.substring(0,subm+1);
-        snum = snum.substring(subm+1);
-        let  n=parseInt(cur,10);
-        let dn=n%100;
-        let pre='';
-        cur = ''+n;
-        if( cur.length === 3 ){
-            let h=cur.substring(0,1);
-            cur = cur.substring(1,3);
-            pre = ones[h]+' '+millsd[1];
-        }
-        if(dn>0){
-            if(dn<20){ 
-                pre += cur=='0' ? '' : ((pre.length>0?' ':'') + ones[cur]);
-            }else{
-                let t = cur.substring(0,1);
-                let s = cur.substring(1,2);
-                pre += (pre.length>0?' ':'') + tens[t] + (s=='0' ? '' : (' '+ ones[s]));
+            if(dn>0){
+                if(dn<20){ 
+                    pre += cur=='0' ? '' : ((pre.length>0?' ':'') + ones[cur]);
+                }else{
+                    let t = cur.substring(0,1);
+                    let s = cur.substring(1,2);
+                    pre += (pre.length>0?' ':'') + tens[t] + (s=='0' ? '' : (' '+ ones[s]));
+                }
             }
+            if( pre.length>0 && millsd[i].length>0) pre += ' '+ millsd[i];
+    
+            if( ret.length>0)
+                ret += ' ' + pre;
+            else
+                ret = pre;
+                
+            --mills;
+            subm=2;
         }
-        if( ret.length>0)
-            ret += ' ' + pre;
-        else
-            ret = pre;
-        --mills;
-        subm=2;
     }
+
     console.log(ret);
 }
 
